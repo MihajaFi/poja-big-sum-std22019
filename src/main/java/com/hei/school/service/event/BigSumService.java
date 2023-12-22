243bbde7c4a6543fbf98b6cad0f9b52881066510
Fifaliana <127596566+MihajaFi@users.noreply.github.com>
2023-12-22 09:09:03 +0300

    feat: add a new controller big-sum

---
+++ src/main/java/com/hei/school/service/event/BigSumService.java
@@ -0,0 +1,14 @@
+package com.hei.school.service.event;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+
+@Service
+public class BigSumService {
+    public BigInteger add(String a, String b) {
+        BigInteger bigA = new BigInteger(a);
+        BigInteger bigB = new BigInteger(b);
+        return bigA.add(bigB);
+    }
+}
